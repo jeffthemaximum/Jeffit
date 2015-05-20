1fb120d42715ad7c5616c4d63a1eede8fb8f956e
Jeff Maxim <frey.maxim@gmail.com>
2015-05-20 19:15:36 +0000

    page reloads to most recently upvoted/downvoted link upon liking/disliking

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -64,13 +64,13 @@ class LinksController < ApplicationController
   def upvote
     @link = Link.find(params[:id])
     @link.upvote_by current_user
-    redirect_to :back
+    redirect_to links_path(:anchor => @link.id)
   end
   
   def downvote
     @link = Link.find(params[:id])
     @link.downvote_by current_user
-    redirect_to :back
+    redirect_to links_path(:anchor => @link.id)
   end
 
   def url_with_protocol(url)
